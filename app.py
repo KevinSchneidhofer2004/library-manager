7fd421cfd3a442198f42cacbda191f5e245f450e
KevinSchneidhofer2004 <schnei190114@sr.htlweiz.at>
2023-01-27 12:26:21 +0100

    update is trash, but almost finished with crud

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///G:/Schule/INSY Projekt/library-manager/test.db'
 
@@ -18,20 +19,72 @@ class Book(db.Model):
     release_year = db.Column(db.Integer)
     customer_id = db.Column(db.Integer)
 
+    def __init__(self, title, author_name, publisher_name, release_year):
+        self.title = title
+        self.author_name = author_name
+        self.publisher_name = publisher_name
+        self.release_year = release_year
+
 
 @app.route('/')
 def index():
-    try:
-        books = Book.query.filter_by(author_name='James Dashner').order_by(Book.title).all()
-        book_text = '<ul>'
-        for book in books:
-            book_text += '<li>' + book.title + ', ' + book.author_name + '</li>'
-        book_text += '</ul>'
-        return book_text
-    except Exception as e:
-        error_text = "<p>The error:<br>" + str(e) + "</p>"
-        hed = '<h1>Something is broken.</h1>'
-        return hed + error_text
+    return render_template('index.html', book = Book.query.all())
+
+@app.route('/new_book', methods = ['GET', 'POST'])
+def new():
+   if request.method == 'POST':
+      if not request.form['title'] or not request.form['author'] or not request.form['publisher'] or not request.form['release_year']:
+         flash('Please enter all the fields', 'error')
+      else:
+         book = Book(request.form['title'], request.form['author'],
+            request.form['publisher'], request.form['release_year'])
+         
+         db.session.add(book)
+         db.session.commit()
+         
+         flash('Record was successfully added')
+         return redirect(url_for('index'))
+   return render_template('new_book.html')
+
+
+@app.route('/<int:id>')
+def RetrieveSingleEmployee(id):
+    book = Book.query.filter_by(id=id).first()
+    if book:
+        return render_template('book_data.html', book = book)
+    return f"Book with id ={id} doesn't exist"
+
+
+@app.route('/<int:id>/update', methods = ['GET','POST'])
+def update(id):
+    book = Book.query.filter_by(id=id).first()
+    if request.method == 'POST':
+        if book:
+            db.session.delete(book)
+            db.session.commit()
+            title = request.form['title']
+            author = request.form['author']
+            publisher = request.form['publisher']
+            release_year = request.form['release_year']
+            book = Book(id=id, title=title, author_name=author, publisher_name=publisher, release_year=release_year)
+            db.session.add(book)
+            db.session.commit()
+            return redirect(f'/{id}')
+        return f"Book with id = {id} Doesn't exist"
+ 
+    return render_template('update.html', book = book)
+
+
+@app.route('/<int:id>/delete', methods=['GET','POST'])
+def Delete(id):
+    book = Book.query.filter_by(id=id).first()
+    if request.method == 'POST':
+        if book:
+            db.session.delete(book)
+            db.session.commit()
+            return redirect('/')
+    return render_template('delete.html')
+
 
 
 app.run(debug=True)
--- book.py
+++ book.py
@@ -6,7 +6,6 @@ from base import Base
 
 class Book(Base):
     __tablename__ = 'books'
-
     id = Column(Integer, primary_key=True)
     title = Column(String)
     author_name = Column(String)
